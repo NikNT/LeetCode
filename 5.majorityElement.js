7682f96ba739c4ed69c212d3d53786ecfa47490f
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-07-11 22:08:29 -0400

    Majority Element

---
+++ 5.majorityElement.js
@@ -0,0 +1,22 @@
+const nums = [3, 2, 3, 4, 4, 4];
+
+const elementCount = {};
+
+var majorityElement = function (nums) {
+  let count = 0;
+  let candidate = null;
+
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+      count++;
+    } else if (candidate === num) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return candidate;
+};
+
+console.log(majorityElement(nums));
